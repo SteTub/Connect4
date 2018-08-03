1954f86615d0a8e0dbf46ab0f4c09611b195a3c4
Stephen Tubbert <stetubbert@gmail.com>
2018-08-03 13:20:46 +0100

    Added weighted column choice

--- Player.cc
+++ Player.cc
@@ -12,28 +12,26 @@ Player* Player::createPlayer(char choice, char tile, int order)
 
 Player::~Player() {}
 
-Human::Human(char tile, int number)
+Human::Human(char tile, int player_num)
 {
 	 my_tile=tile;
-	 player_no=number;
+	 player_no=player_num;
 }
 
-void Human::makeMove(const Board &Game)
+void Human::makeMove(const Board &board)
 {
 	int selection;
 	std::cout << "Player " << player_no << "\nChoose a column: ";
 	std::cin>>selection;
  	
 	//error check selection
-	while(selection < 1 || selection > Game.col_size() || Game.columnFull(selection)==1){
+	while(selection < 1 || selection > board.getNumberOfCols() || board.columnFull(selection)==1){
 		std::cout << "Error: Please select a valid column\n";
 		std::cin>>selection;
 	}
-	
-	Game.insertInColumn(selection,my_tile);
-	Game.fancyPrint(selection, my_tile);
+	board.insertInColumn(selection,my_tile);
+	board.fancyPrint(selection, my_tile);
 }
-
 //=======AI PLAYER=======//
 
 //---CONSTRUCTOR---//
@@ -49,60 +47,92 @@ char AI_player::returnMyTile() const{
 }
 
 //---MAKES THE MOVE USING STRATEGY---//
-void AI_player::makeMove(const Board &Game)
+void AI_player::makeMove(const Board &board)
 {	
 	//choose column based on strategy
-	int selection = chooseColumnUsingStrategy(Game);
+	int selection = chooseColumnUsingStrategy(board);
 
 	//if best column choice is full, select random column
-	while(Game.columnFull(selection)){
-		selection = 1+(rand() %  Game.col_size());
+	while(board.columnFull(selection)){
+		selection = 1+(rand() %  board.getNumberOfCols());
 	}
-	
-	//insert into board
 	usleep(400000);
-	Game.insertInColumn(selection,my_tile);
-	Game.fancyPrint(selection, my_tile);
+	board.insertInColumn(selection,my_tile);
+	board.fancyPrint(selection,my_tile);
 }
 
 //---DETERMINE STRATEGY---//
-int AI_player::chooseColumnUsingStrategy(const Board &Game)
+int AI_player::chooseColumnUsingStrategy(const Board &board)
 {
-	int ncols = Game.col_size();
+	int ncols = board.getNumberOfCols();
 	int col = 0;
-	char enemy_tile = Game.firstTile();
+	char enemy_tile = board.getFirstTile();
 	if(player_no == 1)
-		enemy_tile = Game.secondTile();
+		enemy_tile = board.getSecondTile();
 
 	//TRY TO WIN
-	if(findWinningMove(Game, my_tile) < ncols+1){		//if found 3 and can win, will win
-		col = findWinningMove(Game, my_tile);
+	if(findWinningMove(board, my_tile) < ncols+1){		//if found 3 and can win, will win
+		col = findWinningMove(board, my_tile);
 	}	
 
 	//BLOCK AN OPPONENTS WIN
-	else if(findWinningMove(Game, enemy_tile) < ncols+1){	//if opponent has 3 in row, block opponents win
-		col = findWinningMove(Game, enemy_tile);
+	else if(findWinningMove(board, enemy_tile) < ncols+1){	//if opponent has 3 in row, block opponents win
+		col = findWinningMove(board, enemy_tile);
 	}	
 	//Prevent a 3 in a horizontal row
-	else if(findHorizontalPair(Game, enemy_tile) < ncols+1){	//cut off opponent trap, if opponent has 2 in a row, will play next to one
-		col = findHorizontalPair(Game, enemy_tile);
-	
-		if(choiceWouldCauseLoss(Game, col+1, my_tile, enemy_tile)){	//avoid giving opponent the win
+	else if(findHorizontalPair(board, enemy_tile) < ncols+1){	//cut off opponent trap, if opponent has 2 in a row, will play next to one
+		col = findHorizontalPair(board, enemy_tile);
+
+		if(choiceWouldCauseLoss(board, col+1)){	//avoid giving opponent the win
 			col = 0;
 		}
 	}
 	//If the above three fail then picks random	
 	if(col == 0){
-		col = chooseRandomColumnGaussian(Game, my_tile, enemy_tile);
+		col = chooseRandomCol_Weighted(board);
 	} 
 
 	return col;
 }
 
 //---IF NO OTHER OPTION, PRIORITIZES MIDDLE---//
-int AI_player::chooseRandomColumnGaussian(const Board &Game, char tile, char enemy_tile)
+int AI_player::chooseRandomCol_Weighted(const Board &board)
 {
-	int ncols = Game.col_size();
+	//set up column weights
+	//values come from binomial distribution with n=6 and p=0.5
+	int ncols = board.getNumberOfCols();
+	std::array<int, 7> column_weights = {5,30,75,120,75,30,5};
+	
+	//if column is full or would result in loss then set weight to zero 
+	for(int column=1;column<=ncols;++column){
+
+		if(board.columnFull(column))
+			column_weights[column-1] = 0;
+
+		else if(choiceWouldCauseLoss(board,column))
+			column_weights[column-1] = 0;	
+	}
+	//if there are no possible outcomes, it will play a losing tile
+	if(std::accumulate(column_weights.begin(),column_weights.end(),0)==0){
+	
+		for(int column=1;column<=ncols;++column){
+			if(choiceWouldCauseLoss(board,column))
+				column_weights[column-1] = 100;
+		}		
+	}
+
+	std::discrete_distribution<int> column_weights_dist(column_weights.begin(), column_weights.end());
+	std::random_device rd;
+	std::mt19937 generator(rd());
+
+	int col_choice = column_weights_dist(generator);
+
+	return col_choice+1;		//change column to human readability
+}
+
+int AI_player::chooseRandomCol_Gaussian(const Board &board)
+{
+	int ncols = board.getNumberOfCols();
 	int mid = ncols/2;
 	int col = ncols;
 
@@ -119,12 +149,12 @@ int AI_player::chooseRandomColumnGaussian(const Board &Game, char tile, char ene
 	else {			
 		col = std::round(distrib(generator));	
 		
-		while(Game.columnFull(col) && !(col >= 0) && !(col <= ncols-1))
+		while(board.columnFull(col) && !(col >= 0) && !(col <= ncols-1))
 			col = std::round(distrib(generator));	
 	}
 
 	//check if chosen column would create winning scenario for opponent
-	while(choiceWouldCauseLoss(Game, col+1, tile, enemy_tile))
+	while(choiceWouldCauseLoss(board, col+1))
 	{
 		int count=0;
 		col = std::round(distrib(generator));
@@ -135,32 +165,39 @@ int AI_player::chooseRandomColumnGaussian(const Board &Game, char tile, char ene
 	}
 
 	return col+1;		//change column to human readability
-
 }
 
 //---CHECK IF CHOSEN COLUMN CREATES A WINNING SCENARIO FOR OPPONENT---//
-int AI_player::choiceWouldCauseLoss(const Board &Game, int col, char tile, char enemy_tile)
+int AI_player::choiceWouldCauseLoss(const Board &board, int col)
 {
-	Board test_game = Game;
-	test_game.insertInColumn(col, tile);
+	//get opponents tile
+	char enemy_tile;
+	if(player_no == 1)
+		enemy_tile = board.getSecondTile();
+	else if(player_no == 2)
+		enemy_tile = board.getFirstTile();
+
+	//enter current selection in test board
+	Board test_board = board;
+	test_board.insertInColumn(col, my_tile);
 
 	//CHECK FOR WIN
-	if(findWinningMove(test_game, enemy_tile) < test_game.col_size())		//if found 3 and can win, will win
+	if(findWinningMove(test_board, enemy_tile) <= test_board.getNumberOfCols())		//if found 3 and can win, will win
 		return 1;
 	
 	return 0;
 }
 
 //Find two tiles in a row to set up or stop a trap
-int AI_player::findHorizontalPair(const Board &Game, char tile)
+int AI_player::findHorizontalPair(const Board &board, char tile)
 {
-	int row = Game.row_size();
-	int col = Game.col_size();
+	int row = board.getNumberOfRows();
+	int col = board.getNumberOfCols();
 
 	for(int i=1;i<=row;i++){
-		for(int j=2;j<(col-3);j++){
+		for(int j=2;j<=(col-2);j++){
 			//if find a horizontal pair with space on either side
-			if(Game(i,j) == tile && Game(i,j+1) == tile && Game(i,j-1)==' '&& Game(i,j+2)==' '){
+			if(board(i,j) == tile && board(i,j+1) == tile && board(i,j-1)==' '&& board(i,j+2)==' '){
 				//bottom row
 				if(i==1){
 					//prioritize the space closer to the middle
@@ -170,12 +207,12 @@ int AI_player::findHorizontalPair(const Board &Game, char tile)
 				}
 				else if(i!=1){
 					//if both sides are playable same as case on bottom row
-					if(Game(i-1,j-1)!=' ' && Game(i-1,j+2)!=' '){
+					if(board(i-1,j-1)!=' ' && board(i-1,j+2)!=' '){
 						if(abs(4-(j-1))>abs(4-(j+2)))
 							return j+2;
 						else return j-1;
 					}
-					else if(Game(i-1, j-1) != ' ')
+					else if(board(i-1, j-1) != ' ')
 						return j-1;
 					else
 						return j+2;
@@ -183,26 +220,26 @@ int AI_player::findHorizontalPair(const Board &Game, char tile)
 			}
 		}
 	}
-	return Game.col_size()+1;
+	return board.getNumberOfCols()+1;
 }
 //---FIND THREE TILES WHERE FOURTH TILE WOULD WIN---//
 //This is just a sea of if-else statements
-int AI_player::findWinningMove(const Board &Game, char tile)
+int AI_player::findWinningMove(const Board &board, char tile)
 {
-	int nrows = Game.row_size();
-	int ncols = Game.col_size();
+	int nrows = board.getNumberOfRows();
+	int ncols = board.getNumberOfCols();
 	
 	//Horizontal, free on right
 	for(int i=1;i<=nrows;++i){
 		for(int j=1;j<=(ncols-3);++j){
-			if(Game(i,j)==tile && Game(i,j+1)==tile && Game(i,j+2)==tile){
+			if(board(i,j)==tile && board(i,j+1)==tile && board(i,j+2)==tile){
 				//bottom row
 				if(i==1){
-					if(Game(i,j+3)==' ')
+					if(board(i,j+3)==' ')
 						return j+3;	
 				}
 				else{
-					if(Game(i,j+3)==' ' && Game(i-1,j+3)!=' ')
+					if(board(i,j+3)==' ' && board(i-1,j+3)!=' ')
 						return j+3;
 				}
 			}
@@ -211,14 +248,14 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	//Horizontal, free on left
 	for(int i=1;i<=nrows;++i){
 		for(int j=2;j<(ncols-1);++j){
-			if(Game(i,j)==tile && Game(i,j+1)==tile && Game(i,j+2)==tile){		
+			if(board(i,j)==tile && board(i,j+1)==tile && board(i,j+2)==tile){		
 				//bottom row
 				if(i==1){
-					if(Game(i,j-1)==' ')
+					if(board(i,j-1)==' ')
 						return j-1;
 				}
 				else{
-					if(Game(i,j-1)==' ' && Game(i-1,j-1)!=' ')
+					if(board(i,j-1)==' ' && board(i-1,j-1)!=' ')
 						return j-1;
 				}
 			}
@@ -228,28 +265,28 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	for(int i=1;i<=nrows;++i){
 		for(int j=1;j<=(ncols-3);++j){
 			//if OO_O
-			if(Game(i,j) == tile && Game(i,j+1) == tile && Game(i,j+3) == tile){
+			if(board(i,j) == tile && board(i,j+1) == tile && board(i,j+3) == tile){
 				//bottom row
 				if(i==1){
-					if(Game(i,j+2) == ' ')
+					if(board(i,j+2) == ' ')
 						return j+2;	
 				}
 				//not on bottom row
 				else{	
-					if(Game(i-1,j+2) != ' ' && Game(i,j+2) == ' ')
+					if(board(i-1,j+2) != ' ' && board(i,j+2) == ' ')
 						return j+2;	
 				}
 			}
 			//if O_OO
-			if(Game(i,j) == tile && Game(i,j+2) == tile && Game(i,j+3) == tile){
+			if(board(i,j) == tile && board(i,j+2) == tile && board(i,j+3) == tile){
 				//bottom row
 				if(i==1){
-					if(Game(i, j+1) == ' ')
+					if(board(i, j+1) == ' ')
 						return j+1;	
 				}
 				//not on bottom row
 				else{	
-					if(Game(i-1, j+1) != ' ' && Game(i, j+1) == ' ')
+					if(board(i-1, j+1) != ' ' && board(i, j+1) == ' ')
 						return j+1;	
 				}
 			}
@@ -259,8 +296,8 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	//VERTICAL
 	for(int j=1;j<=ncols;++j){
 		for(int i=1;i<=(nrows-3);++i){
-			if(Game(i,j) == tile && Game(i+1,j) == tile && Game(i+2,j) == tile){
-				if(Game(i+3,j) == ' ')
+			if(board(i,j) == tile && board(i+1,j) == tile && board(i+2,j) == tile){
+				if(board(i+3,j) == ' ')
 					return j;	
 			}			
 		}
@@ -270,22 +307,22 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	//diagonal up together - top right
 	for(int i=1;i<=(nrows-3);++i){
 		for(int j=1;j<=(ncols-3);++j){
-			if(Game(i,j) == tile && Game(i+1,j+1) == tile && Game(i+2,j+2) == tile){
-				if(Game(i+3,j+3)==' ' && Game(i+2,j+3)!=' ')
+			if(board(i,j) == tile && board(i+1,j+1) == tile && board(i+2,j+2) == tile){
+				if(board(i+3,j+3)==' ' && board(i+2,j+3)!=' ')
 					return j+3;		
 			}
 		}
 	}
 	for(int i=2;i<=(nrows-2);++i){
 		for(int j=2;j<=(ncols-2);++j){
-			if(Game(i,j) == tile && Game(i+1,j+1) == tile && Game(i+2,j+2) == tile){
+			if(board(i,j) == tile && board(i+1,j+1) == tile && board(i+2,j+2) == tile){
 				//bottom row
 				if(i==2){
-					if(Game(i-1,j-1)==' ')
+					if(board(i-1,j-1)==' ')
 						return j-1; 
 				}
 				else{
-					if(Game(i-1,j-1)==' ' && Game(i-2,j-1)!=' ')
+					if(board(i-1,j-1)==' ' && board(i-2,j-1)!=' ')
 						return j-1; 
 				}
 			}
@@ -296,14 +333,14 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	for(int i=1;i<=(nrows-3);++i){
 		for(int j=1;j<(ncols-2);++j){
 			//O_OO
-			if(Game(i,j) == tile && Game(i+2,j+2) == tile && Game(i+3,j+3) == tile){
-				if(Game(i+1, j+1)==' ' && Game(i, j+1) !=' ' ){
+			if(board(i,j) == tile && board(i+2,j+2) == tile && board(i+3,j+3) == tile){
+				if(board(i+1, j+1)==' ' && board(i, j+1) !=' ' ){
 					return j+1;	
 				}
 			}
 			//OO_O
-			if(Game(i,j) == tile && Game(i+1,j+1) == tile && Game(i+3,j+3) == tile){
-				if(Game(i+2, j+2)==' ' && Game(i+1,j+2)!=' ' ){
+			if(board(i,j) == tile && board(i+1,j+1) == tile && board(i+3,j+3) == tile){
+				if(board(i+2, j+2)==' ' && board(i+1,j+2)!=' ' ){
 					return j+2;	
 				}
 			}
@@ -315,15 +352,15 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	//diagonal down together open bottom right
 	for(int i=4;i<=nrows;++i){
 		for(int j=1;j<=(ncols-3);++j){
-			if(Game(i,j)==tile && Game(i-1,j+1)==tile && Game(i-2,j+2)==tile){
+			if(board(i,j)==tile && board(i-1,j+1)==tile && board(i-2,j+2)==tile){
 				//bottom row
 				if(i==4){
-					if(Game(i-3,j+3)==' ')
+					if(board(i-3,j+3)==' ')
 						return j+3;
 				}			
 				//not bottom row
 				else{
-					if(Game(i-3,j+3)==' ' && Game(i-4,j+3)!=' ')
+					if(board(i-3,j+3)==' ' && board(i-4,j+3)!=' ')
 						return j+3;
 				}
 			}	
@@ -334,8 +371,8 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	//diagonal down together, top left
 	for(int i=3;i<=(nrows-1);++i){
 		for(int j=2;j<(ncols-1);++j){	
-			if(Game(i,j)==tile && Game(i-1,j+1)==tile && Game(i-2,j+2)==tile){
-				if(Game(i+1,j-1)==' ' && Game(i,j-1)!=' ')
+			if(board(i,j)==tile && board(i-1,j+1)==tile && board(i-2,j+2)==tile){
+				if(board(i+1,j-1)==' ' && board(i,j-1)!=' ')
 					return j-1;
 			}
 		}
@@ -345,14 +382,14 @@ int AI_player::findWinningMove(const Board &Game, char tile)
 	for(int i=4;i<=nrows;++i){
 		for(int j=1;j<=(ncols-3);++j){
 			//O_OO
-			if(Game(i,j) == tile && Game(i-2,j+2) == tile && Game(i-3,j+3) == tile){
-				if(Game(i-1, j+1)==' ' && Game(i-2, j+1)!=' '){
+			if(board(i,j) == tile && board(i-2,j+2) == tile && board(i-3,j+3) == tile){
+				if(board(i-1, j+1)==' ' && board(i-2, j+1)!=' '){
 					return j+1;	
 				}
 			}
 			//OO_O
-			if(Game(i,j) == tile && Game(i-1,j+1) == tile && Game(i-3,j+3) == tile){
-				if(Game(i-2, j+2)==' ' && Game(i-3, j+2)!=' ' ){
+			if(board(i,j) == tile && board(i-1,j+1) == tile && board(i-3,j+3) == tile){
+				if(board(i-2, j+2)==' ' && board(i-3, j+2)!=' ' ){
 					return j+2;	
 				}
 			}
--- Player.h
+++ Player.h
@@ -2,11 +2,12 @@
 #include<unistd.h>
 #include<cmath>
 #include<random>
+#include<array>
 
 class Player {
 	public:
 		virtual ~Player()=0;
-		virtual void makeMove(const Board &Game)=0;
+		virtual void makeMove(const Board &board)=0;
 		static Player* createPlayer(char choice,char tile,int order);
 };
 
@@ -14,16 +15,17 @@ class AI_player : public Player{
 
 	public:
 		//Constructor
-		AI_player(char tile, int player_no);
+		AI_player(char tile, int player_num);
 		//virtual destructor
 		~AI_player() {};
 		
-		int chooseRandomColumnGaussian(const Board &Game, char tile, char enemy_tile);
-		int choiceWouldCauseLoss(const Board &Game, int col, char tile, char enemy_tile);
-		int findHorizontalPair(const Board &Game, char tile);	//finds pair on same row to prevent a 3 in a row win on either side
-		int findWinningMove(const Board &Game, char tile);
-		int chooseColumnUsingStrategy(const Board &Game);	
-		void makeMove(const Board &Game);
+		int chooseRandomCol_Weighted(const Board &board);
+		int chooseRandomCol_Gaussian(const Board &board);
+		int choiceWouldCauseLoss(const Board &board, int col);
+		int findHorizontalPair(const Board &board, char tile);	//finds pair on same row to prevent a 3 in a row win on either side
+		int findWinningMove(const Board &board, char tile);
+		int chooseColumnUsingStrategy(const Board &board);	
+		void makeMove(const Board &board);
 		char returnMyTile() const;					//determines own tile
 
 	private:
@@ -34,9 +36,9 @@ class AI_player : public Player{
 class Human : public Player {
 
 	public:
-		Human(char tile, int number);
+		Human(char tile, int player_num);
 		~Human() {};
-		void makeMove(const Board &Game);	
+		void makeMove(const Board &board);	
 
 	private:
 		char my_tile;	
